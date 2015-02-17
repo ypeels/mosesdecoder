3dcd69b8d713d83e3fc414a09ac3de7b8b8cf145
Hieu Hoang <hieuhoang@gmail.com>
2015-02-17 14:54:48 +0000

    training for LM01

--- contrib/lm01/compile.sh
+++ contrib/lm01/compile.sh
@@ -1,2 +1,4 @@
 g++ -I../../ -I../../boost/include -L../../boost/lib64 -o extract extract.cpp ../../phrase-extract/InputFileStream.cpp ../../phrase-extract/OutputFileStream.cpp -lz -lboost_iostreams
 
+g++ -I../../ -I../../boost/include -L../../boost/lib64 -o score score.cpp ../../phrase-extract/InputFileStream.cpp ../../phrase-extract/OutputFileStream.cpp -lz -lboost_iostreams
+
