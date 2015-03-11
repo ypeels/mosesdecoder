eae5863437dff1b0e51ba05089a0bac542936ac3
Hieu Hoang <hieuhoang@gmail.com>
2015-03-11 21:30:06 +0000

    create walls and zones

--- contrib/other-builds/manual-label/ZonesAndWalls.cpp
+++ contrib/other-builds/manual-label/ZonesAndWalls.cpp
@@ -7,53 +7,14 @@ using namespace std;
 
 void ZonesAndWalls(const Phrase &source, std::ostream &out)
 {
-	ZonesAndWalls(source, out, "&quot;", "&quot;");
-}
-
-void ZonesAndWalls(const Phrase &source, std::ostream &out, const std::string &start, const std::string &end)
-{
-	stack<size_t> startStack;
-	typedef pair<size_t,size_t> Zone;
-	typedef set<Zone> Zones;
 	Zones zones;
 
-	size_t size = source.size();
-	for (size_t i = 0; i < size; ++i) {
-		const Word &word = source[i];
-		assert(word.size() == 1);
-		const string factor = word[0];
-
-		if (start == end) {
-			if (factor ==  start) {
-				if (startStack.empty()) {
-					startStack.push(i);
-				}
-				else {
-					size_t startPos = startStack.top();
-					startStack.pop();
-
-					Zone zone(startPos, i);
-					zones.insert(zone);
-				}
-			}
-		}
-		else {
-			if (factor == start) {
-				startStack.push(i);
-			}
-			else if (factor == end) {
-				if (!startStack.empty()) {
-					size_t startPos = startStack.top();
-					startStack.pop();
-
-					Zone zone(startPos, i);
-					zones.insert(zone);
-				}
-			}
-		}
-	}
+	ZonesAndWalls(source, out, "&quot;", "&quot;", zones);
+	ZonesAndWalls(source, out, "(", ")", zones);
+	ZonesAndWalls(source, out, "&#91;", "&#93;", zones);
 
 	// output
+	size_t size = source.size();
 	for (size_t i = 0; i < size; ++i) {
 		// before outputting word
 		Zones::const_iterator iter;
@@ -91,5 +52,51 @@ void ZonesAndWalls(const Phrase &source, std::ostream &out, const std::string &s
 	}
 
 	out << endl;
+
+}
+
+void ZonesAndWalls(const Phrase &source, std::ostream &out,
+		const std::string &start,
+		const std::string &end,
+		Zones &zones)
+{
+	stack<size_t> startStack;
+
+	size_t size = source.size();
+	for (size_t i = 0; i < size; ++i) {
+		const Word &word = source[i];
+		assert(word.size() == 1);
+		const string factor = word[0];
+
+		if (start == end) {
+			if (factor ==  start) {
+				if (startStack.empty()) {
+					startStack.push(i);
+				}
+				else {
+					size_t startPos = startStack.top();
+					startStack.pop();
+
+					Zone zone(startPos, i);
+					zones.insert(zone);
+				}
+			}
+		}
+		else {
+			if (factor == start) {
+				startStack.push(i);
+			}
+			else if (factor == end) {
+				if (!startStack.empty()) {
+					size_t startPos = startStack.top();
+					startStack.pop();
+
+					Zone zone(startPos, i);
+					zones.insert(zone);
+				}
+			}
+		}
+	}
+
 }
 
--- contrib/other-builds/manual-label/ZonesAndWalls.h
+++ contrib/other-builds/manual-label/ZonesAndWalls.h
@@ -2,5 +2,11 @@
 
 #include "Main.h"
 
+typedef std::pair<size_t,size_t> Zone;
+typedef std::set<Zone> Zones;
+
 void ZonesAndWalls(const Phrase &source, std::ostream &out);
-void ZonesAndWalls(const Phrase &source, std::ostream &out, const std::string &start, const std::string &end);
+void ZonesAndWalls(const Phrase &source, std::ostream &out,
+		const std::string &start,
+		const std::string &end,
+		Zones &zones);
