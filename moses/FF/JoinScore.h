ba3a0c413b512cccfea7cf70f26a56ba8601cb53
Hieu Hoang <hieuhoang@gmail.com>
2015-06-16 14:39:15 +0400

    don't use unordered map. c++11 issues

--- moses/FF/JoinScore.h
+++ moses/FF/JoinScore.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <unordered_map>
+#include <map>
 #include <string>
 #include "StatefulFeatureFunction.h"
 #include "FFState.h"
@@ -52,7 +52,7 @@ class JoinScore : public StatefulFeatureFunction
     const Node *Find(const std::string &tok) const;
 
   protected:
-    typedef std::unordered_map<char, Node*> Children;
+    typedef std::map<char, Node*> Children;
     Children m_children;
 
     Node *Insert(const std::string &tok, size_t pos);
