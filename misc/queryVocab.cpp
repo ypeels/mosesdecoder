435de283ea5c1d767b7beabadb53664333b4be6c
Hieu Hoang <hieuhoang@gmail.com>
2015-07-13 09:22:47 +0400

    format output from querying vocab

--- misc/queryVocab.cpp
+++ misc/queryVocab.cpp
@@ -4,29 +4,40 @@
 using namespace std;
 using namespace Moses;
 
+//! delete white spaces at beginning and end of string
+inline std::string Trim(const std::string& str, const std::string dropChars = " \t\n\r")
+{
+  std::string res = str;
+  res.erase(str.find_last_not_of(dropChars)+1);
+  return res.erase(0, res.find_first_not_of(dropChars));
+}
+
 int main(int argc, char* argv[])
 {
 	cerr << "Starting..." << endl;
 
-  string inPath(argv[1]);
+  string modelPath(argv[1]);
   TrieSearch<bool> trieSearch;
-  trieSearch.Create(inPath);
+  trieSearch.Create(modelPath);
 	
 	// MAIN LOOP
 	string line;
 	while (getline(cin, line)) {
+		line = Trim(line);
   	if (line.empty()) {
 			continue;
 		}
 
+		cout << line << " ";
 		bool value;
 		bool found = trieSearch.Find(value, line);
 		if (found) {
-			cerr << "FOUND=" << value << endl;	
+			//cout << "FOUND=" << value << endl;	
 		}
 		else {
-			cerr << "NOT FOUND" << endl;	
-		}		
+			//cout << "NOT FOUND" << endl;	
+		}
+		cout << found << " " << value << endl;
 	}
 	
 	cerr << "Finished" << endl;
