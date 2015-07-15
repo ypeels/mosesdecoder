664e3426255106a0f78f15d9fe1c7ac055a6a606
Hieu Hoang <hieuhoang@gmail.com>
2015-07-15 12:48:14 +0400

    don't use InputFileStream. Compiles

--- contrib/other-builds/diff-with-ref/Main.h
+++ contrib/other-builds/diff-with-ref/Main.h
@@ -1,13 +1,13 @@
 #pragma once
 
+#include <fstream>
 #include <vector>
 #include <map>
 #include <string>
-#include "moses/InputFileStream.h"
 
 typedef std::map<std::string, int> Counts;
 
-void Compare(Moses::InputFileStream &refStrme, Moses::InputFileStream &in1Strme, Moses::InputFileStream &in2Strme);
+void Compare(std::ifstream &refStrme, std::ifstream &in1Strme, std::ifstream &in2Strme);
 void Compare(const std::vector<std::string> &toksRef, const std::vector<std::string> &toksIn1, const std::vector<std::string> &toksIn2);
 void AddToCounts(Counts &counts, const std::vector<std::string> &toks);
 bool SubtractFromCounts(Counts &counts, const std::string &tok);
