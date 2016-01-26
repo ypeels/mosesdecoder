bce950878f50f730a73409aa3ba2bca54aa15a31
Hieu Hoang <hieuhoang@gmail.com>
2016-01-26 18:50:28 +0000

    fixing segfault on thrud

--- contrib/other-builds/moses2/legacy/Util2.h
+++ contrib/other-builds/moses2/legacy/Util2.h
@@ -39,7 +39,7 @@ public:
 };
 
 template<typename T>
-void Init(T arr[], size_t size, const T &val) {
+inline void Init(T arr[], size_t size, const T &val) {
 	for (size_t i = 0; i < size; ++i) {
 		arr[i] = val;
 	}
